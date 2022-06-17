1610c95c0df8de434125899a20f199677bd904bc
unknown <shuklapradyuman786@gmail.com>
2022-06-17 16:45:50 +0530

    overstock

---
+++ shop.js
@@ -0,0 +1,267 @@
+//image
+//fname
+//Type
+//Price
+//description
+// -----------------------------------   //
+// sidebar  //
+
+
+
+
+
+
+
+
+
+// let BodyCareProducts = [];
+
+// class Category {
+
+//     constructor(img,fname,type,price,description){
+
+//         this.img = img;
+//         this.fname = fname;
+//         this.type = type;
+//         this.price = price;
+//         this.description = description;
+//     }
+// }
+
+// class products extends Category{
+
+//     constructor(img,fname,type,price,description){
+
+//         super(img,fname,type,price,description);
+//     }
+// }
+
+// let c1 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Butterfly","Fine Fragrance",17.50,"Mix & Match: Buy 3, Get 1 Free")
+// let c2 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwe52c7f04/crop/026403504_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Sandalwood Eucalyptus","Body Wash and Foam Bath",15.50,"Mix & Match: Buy 3, Get 1 Free");
+// let c3 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwff743235/crop/026438530_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Japanese Cherry Blossom","Ultimate Hydration Body Cream",15.50,"$5.95 Today Only! with Code: TIME2RELAX")
+// let c4 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwba77ffbc/crop/026267868_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Ocean","Body Spray",13.50,"Mix & Match: Buy 3, Get 1 Free")
+// let c5 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw81522767/crop/026401914_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Pineapple Lime Eucalyptus","Essential Oil Mist",15.50,"$5.95 Today Only! with Code: TIME2RELAX")
+// let c6 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dweccfb026/crop/026451873_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","White Tea & Sage","Shower Gel",13.50,"Mix & Match: Buy 3, Get 1 Free")
+// let c7 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw65573859/crop/026451884_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Buttercups & Berry Bellini","Fine Fragrance Mist",15.50,"Mix & Match: Buy 3, Get 1 Free")
+// let c8 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwbfc18541/crop/026401907_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Hibiscus Mandarin Violet","Ultimate Hydration Body Cream",17.50,"Mix & Match: Buy 3, Get 1 Free")
+// let c9 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw50a09db0/crop/026395407_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Freshwater","3-in-1 Hair, Face & Body Wash",14.50 ,"$5.95 Today Only! with Code: TIME2RELAX")
+// let c10 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwd097b2cd/crop/026451889_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Honeysuckle & Peach Spritz","Ultimate Hydration Body Cream",15.50 ,"$5.95 Today Only! with Code: TIME2RELAX",)
+// let c11 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc02b92b6/crop/026381189_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Cacao Rose","Body Wash and Foam Bath",15.50 ,"Mix & Match: Buy 3, Get 1 Free")
+// let c12 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7c6a3bb3/crop/026400330_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Strawberry Pound Cake","Fine Fragrance Mist",17.50 ,"Mix & Match: Buy 3, Get 1 Free")
+// let c13 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwaf5dec98/crop/026439080_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Lavender Mint Tea","Ultimate Hydration Body Cream",17.50 ,"Mix & Match: Buy 3, Get 1 Free")
+// let c14 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc189b575/crop/026240389_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","A Thousand Wishes","Super Smooth Body Lotion",14.50 ,"Mix & Match: Buy 3, Get 1 Free")
+// let c15 = new products("https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8a157107/crop/026353232_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_","Lavender Vanilla","Pillow and Body Mist",14.50 ,"$5.95 Today Only! with Code: TIME2RELAX")
+
+// BodyCareProducts.push(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15)
+// //console.log(BodyCareProducts)
+
+
+// BodyCareProducts.forEach(({img,fname,type,price,description}) =>{
+
+//     let container = document.querySelector("#container_care");
+
+//     let dib = document.createElement("div")
+//     dib.setAttribute("class","care_div")
+
+//     let img_div = document.createElement("div")
+
+    
+//     let image = document.createElement("img")
+//     image.src = img;
+
+//     let span = document.createElement("span")
+//     span.setAttribute("class","spaan");
+//     span.innerHTML = '<i class="fa-regular fa-eye"></i><br>' + "QuickLook";
+//     span.style.fontSize = "11px"
+//     span.style.marginLeft = "120px"
+//     //span.style.display = "none"
+//     img_div.append(image,span)
+
+//     let name = document.createElement("h3")
+//     name.innerText = fname;
+
+//     let types = document.createElement("p")
+//     types.innerText = type;
+//     types.style.fontSize = "13px"
+
+//     let prices = document.createElement("h3")
+//     prices.innerText = `$${price}`;
+
+//     let descriptions = document.createElement("p")
+//     descriptions.innerText = description;
+//     descriptions.style.color = "red"
+//     descriptions.style.fontSize = "11px"
+
+
+//     let carebutton = () =>{
+    
+//     }
+
+//     let btn = document.createElement("button")
+//     btn.setAttribute("id","care_button")
+//     btn.innerText = "ADD TO BAG"
+//     btn.style.padding = "10px 80px"
+
+//     btn.addEventListener("click",carebutton)
+
+
+//     let span1 = document.createElement("span")
+//     span1.setAttribute("class","spaan1");
+//     span1.innerHTML = '<span class="fa fa-star checked"></span>' + '<span class="fa fa-star checked"></span>'
+//     + '<span class="fa fa-star checked"></span>' + '<span class="fa fa-star checked"></span>' + '<span class="fa fa-star"></span>';
+
+//     // let span2 = document.createElement("span")
+//     // span2.innerHTML = '(11)'
+
+    
+
+
+    
+
+
+//     dib.append(img_div,name,types,prices,descriptions,btn,span1)
+//     container.append(dib)
+   
+// });
+
+
+// ==================================================NEW================//
+//image
+//fname
+//Type
+//Price
+//description
+
+let BodyCareProducts = [];
+
+class Category {
+
+    constructor(img,fname,type,price,description){
+
+        this.img = img;
+        this.fname = fname;
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+}
+
+class products extends Category{
+
+    constructor(img,fname,type,price,description){
+
+        super(img,fname,type,price,description);
+    }
+}
+
+// let c1 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/325856d566f537c461d422ef4a5213e83daa0428/MALACASA-Series-Bake.Bake%2C-Rectangle-Porcelain-Bakeware-Set.jpg?imwidth=480&impolicy=medium","bucket","soap & bath tab",14.50 ,"$12.95 Today Only! with Code: TIME2RELAX")
+// let c2 = new products("https://ak1.ostkcdn.com/images/products/14988170/20-Piece-Laguiole-Faux-Ivory-Flatware-Set-by-French-Home-29c6f1a4-9482-4bbc-a62e-9d5346cce42b_600.jpg?imwidth=480&impolicy=medium","Newair 44lb Nugget with self-cleaning function","newair 44lb Nugget",44949,"");
+let c3 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/f152c3acf829090d6bae785b1e23ffe842a7f65f/Aluminum-Traditional-Wine-Holder.jpg?imwidth=480&impolicy=medium","","Ultimate Hydration Body Cream",15.50,"$150.95 Today Only! with Code: TIME2RELAX")
+let c4 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/073d6f90ec888c7ea4852c2c7d93c775e7d36928/Innovaze-13-Gallon-Stainless-Steel-Oval-Kitchen-Motion-Sensor-Trash-Can.jpg?imwidth=480&impolicy=medium","Ocean","Body Spray",13.50,"Mix & Match: Buy 3, Get 1 Free")
+let c5 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/d204031e7f876f028ad9a10a4f732d216831bcc1/Design-Imports-Silver-Doubleframe-PVC-Kitchen-Placemat-Set-%28Set-of-6%29.jpg?imwidth=480&impolicy=medium","Pineapple Lime Eucalyptus","Essential Oil Mist",15.50,"$5.95 Today Only! with Code: TIME2RELAX")
+let c6 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/319d6cd5503fc97038e7c72d7d2fa88328c794f9/3-drawer-Drop-Leaf-Kitchen-Cart.jpg?imwidth=480&impolicy=medium","White Tea & Sage","Shower Gel",13.50,"Mix & Match: Buy 3, Get 1 Free")
+let c7 = new products("https://ak1.ostkcdn.com/images/products/27751290/Home-Bakery-Virtuoso-Plus-3663fe9f-7cb6-4feb-a70e-8acf5bfab8ce_600.jpg?imwidth=480&impolicy=medium","Buttercups & Berry Bellini","Fine Fragrance Mist",15.50,"Mix & Match: Buy 3, Get 1 Free")
+let c8 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/a90e168847b157d67246e59b12411cd59a23606f/Stone-Lain-Coupe-Stoneware-Dinnerware-Set.jpg?imwidth=480&impolicy=medium","Hibiscus Mandarin Violet","Ultimate Hydration Body Cream",17.50,"Mix & Match: Buy 3, Get 1 Free")
+let c9 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/ceac47b2384a937af8667079bcb3fff1942dfb21/Kitchen-Academy-12-Piece-Nonstick-Granite-Coated-Cookware-Set---Bakelite-Handle-With-Wood-Effect-%28Soft-Touch%29.jpg?imwidth=480&impolicy=medium","Freshwater","3-in-1 Hair, Face & Body Wash",14.50 ,"$5.95 Today Only! with Code: TIME2RELAX")
+let c10 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/9947ffacc60b2bc4e7d39bebc56d053b94aa1030/Euro-Ceramica-Zanzibar-2-piece-Serving-Set%2C-Platter-and-Bowl.jpg?imwidth=480&impolicy=medium","kitchen table","kitchen tabele",15.50 ,"$5.95 Today Only! with Code: TIME2RELAX",)
+let c11 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/4daefee36632e136a0c1a8c19866b5fbca23ed5f/Elrene-Everyday-Casual-Print-Napkins-Set-of-24.jpg?imwidth=480&impolicy=medium","dryer","air dryer",15.50 ,"Mix & Match: Buy 3, Get 1 Free")
+let c12 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/9dcb0c25879c712b9c5dc7975351d054bc1f950a/Shimmering-Woven-Nubby-Texture-Water-Hyacinth-Table-Runner.jpg?imwidth=480&impolicy=medium","plate & ","plate with color full",17.50 ,"Mix & Match: Buy 3, Get 1 Free")
+let c13 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/957e76202f5b62fbab98f3dfa4babc1262c4aafd/Euro-Ceramica-White-Essential-16-Piece-Porcelain-Dinnerware-Set.jpg?imwidth=480&impolicy=medium","plate","food plate",17.50 ,"Mix & Match: Buy 3, Get 1 Free")
+let c14 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/8158bc44f77ea983542a7f06a18fbd15bb8a6b04/Evelots-Spice-Rack-5-Shelves-Wall-Door-Mount-No-Rust-Easy-Clean-Up-to-40-Bottles.jpg?imwidth=480&impolicy=medium","home ","bottle",14.50 ,"Mix & Match: Buy 3, Get 1 Free")
+let c15 = new products("https://ak1.ostkcdn.com/images/products/is/images/direct/325856d566f537c461d422ef4a5213e83daa0428/MALACASA-Series-Bake.Bake%2C-Rectangle-Porcelain-Bakeware-Set.jpg?imwidth=480&impolicy=medium","bucket","soap & bath tab",14.50 ,"$12.95 Today Only! with Code: TIME2RELAX")
+
+BodyCareProducts.push(c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15)
+//console.log(BodyCareProducts)
+
+
+
+
+BodyCareProducts.forEach(({img,fname,type,price,description}) =>{
+
+    let container = document.querySelector("#container_care");
+
+    let dib = document.createElement("div")
+    dib.setAttribute("class","care_div")
+
+    let img_div = document.createElement("div")
+
+    
+    let image = document.createElement("img")
+    image.src = img;
+
+    let span = document.createElement("span")
+    span.setAttribute("class","spaan");
+    span.innerHTML = '<i class="fa-regular fa-eye"></i><br>' + "QuickLook";
+    span.style.fontSize = "11px"
+    span.style.marginLeft = "120px"
+    //span.style.display = "none"
+    img_div.append(image,span)
+
+    let name = document.createElement("h3")
+    name.innerText = fname;
+
+    let types = document.createElement("p")
+    types.innerText = type;
+    types.style.fontSize = "13px"
+
+    let prices = document.createElement("h3")
+    prices.innerText = `$${price}`;
+
+    let descriptions = document.createElement("p")
+    descriptions.innerText = description;
+    descriptions.style.color = "red"
+    descriptions.style.fontSize = "11px"
+
+
+
+    let btn = document.createElement("button")
+    btn.setAttribute("id","care_button")
+    btn.innerText = "ADD TO BAG"
+    btn.style.padding = "10px 80px"
+
+    btn.addEventListener("click",function(){
+        AddToBag({img,fname,type,price,description})
+
+    })
+
+
+    let span1 = document.createElement("span")
+    span1.setAttribute("class","spaan1");
+    span1.innerHTML = '<span class="fa fa-star checked"></span>' + '<span class="fa fa-star checked"></span>'
+    + '<span class="fa fa-star checked"></span>' + '<span class="fa fa-star checked"></span>' + '<span class="fa fa-star"></span>';
+
+    // let span2 = document.createElement("span")
+    // span2.innerHTML = '(11)'
+
+     dib.append(img_div,name,types,prices,descriptions,btn,span1)
+    container.append(dib)
+
+    img_div.addEventListener("click",function(){
+        DisplayPage({img,fname,type,price,description})
+      
+    })
+   
+});
+
+
+let CartData = JSON.parse(localStorage.getItem("bag")) || [];
+let Display = JSON.parse(localStorage.getItem("display")) || [];
+
+
+let AddToBag = ({img,fname,type,price,description}) =>{
+    // console.log({img,fname,type,price,description});
+    // CartData.push({img,fname,type,price,description});
+    let obj = {img,fname,type,price}
+    localStorage.setItem("bag",JSON.stringify(obj));
+    window.location.href = "cart.html"
+   // window.location.reload();
+}
+
+
+let DisplayPage = ({img,fname,type,price,description}) =>{
+    //console.log({img,fname,type,price,description});
+    Display.push({img,fname,type,price,description});
+    localStorage.setItem("display",JSON.stringify(Display));
+    
+}
+
